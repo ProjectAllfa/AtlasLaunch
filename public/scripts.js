6fa273086bbd319f185b06b1a2858d0c5b6f6853
ProjectAllfa <abcnewzpr@gmail.com>
2025-04-27 11:24:23 +0300

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -2107,6 +2107,14 @@ if (mediaType === 'tv') {
           console.error('Error fetching seasons data:', error);
           episodesContainer.innerHTML = '<p>Error fetching seasons data</p>';
       });
+ }  else {
+              episodesContainer.innerHTML = '<p>No seasons available</p>';
+          }
+      })
+      .catch(error => {
+          console.error('Error fetching seasons data:', error);
+          episodesContainer.innerHTML = '<p>Error fetching seasons data</p>';
+      });
  } else {
   
   seasonDropdown.style.display = 'none'; // Ensure dropdown is hidden
